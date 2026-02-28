795978dd0dbd5c5487f877b5374f95ec2d9da6aa
Victor Emmanuel <victorchukwudionline@gmail.com>
2026-02-28 21:26:33 +0100

    Created the server and database architectures

---
+++ src/models/user.js
@@ -0,0 +1,46 @@
+import mongoose, { SChema } from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema(
+{
+  username: { 
+        type: String, 
+        required: true, 
+        unique: true ,
+        lowercase: true,
+        trim: true,
+        minLength: 1,
+        maxLength: 30
+    },
+
+    email: { 
+        type: String, 
+        required: true, 
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+
+    password: { 
+        type: String, 
+        required: true,
+        minLength: 6,
+        maxLength: 50
+    },
+}, 
+
+{ timestamps: true });
+
+// Hash password before saving
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// Method to compare password for login
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+export const User = mongoose.model( "USer", userSchema)
