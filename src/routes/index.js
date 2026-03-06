ae79c8127bc0aba4aa3e94285eb094c7b85b5e8b
emmystrings911 <sectorloko12@gmail.com>
2026-03-06 22:33:43 +0100

    Add files via upload

---
+++ src/routes/index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+import postRoutes from './post.routes.js';
+import userRoutes from './user.routes.js'
+
+const router = express.Router();
+
+router.use("/posts", postRoutes)
+router.use("/auth", userRoutes)
+
+export default router;
