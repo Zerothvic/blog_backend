2eaa33d3a21a2030f524a741f7085b5ecbfbfe33
emmystrings911 <sectorloko12@gmail.com>
2026-03-15 14:11:48 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -6,7 +6,6 @@ import { fileURLToPath } from "url";
 
 const app = express();
 
-// Fix __dirname in ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
