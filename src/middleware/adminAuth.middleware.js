78849f2389098c7739e8d1d1abfd644c127db3b0
emmystrings911 <sectorloko12@gmail.com>
2026-03-19 20:46:55 +0100

    Delete src/middleware/adminAuth.middleware.js

--- src/middleware/adminAuth.middleware.js
+++
@@ -1,8 +0,0 @@
-export const isAdmin = (req, res, next) => {
-    // Check if the user exists and has the admin role
-    if (req.user && req.user.role === "admin") {
-        next();
-    } else {
-        res.status(403).json({ message: "Forbidden: Admin access only" });
-    }
-};
